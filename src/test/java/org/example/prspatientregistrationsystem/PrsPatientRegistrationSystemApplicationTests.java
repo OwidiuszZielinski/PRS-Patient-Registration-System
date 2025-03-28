6d0beb36f2e923a7b1a400718d6c81b921f7c4fb
Owidiusz Zieliński <zielinski.owidiusz@gmail.com>
2025-03-28 20:42:41 +0100

    Start project

---
+++ src/test/java/org/example/prspatientregistrationsystem/PrsPatientRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prspatientregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrsPatientRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
