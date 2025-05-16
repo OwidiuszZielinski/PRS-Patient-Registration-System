0a2c98375fcf4b647c7af82e7db5fd23a49c68eb
Owidiusz Zieliński <zielinski.owidiusz@gmail.com>
2025-05-17 01:10:35 +0200

    Added insert to DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,5 @@ VALUES ('Jan', 1,  'Kowalski', 'DOC123456'),
        ('Anna', 2, 'Nowak', 'DOC654321'),
        ('Piotr', 3, 'Wiśniewski', 'DOC987654'),
        ('Maria', 4, 'Wójcik', 'DOC456789'),
+       ('Andrzej', 5, 'Kowalczyk', 'DOC321654'),
        ('Andrzej', 5, 'Kowalczyk', 'DOC321654');
